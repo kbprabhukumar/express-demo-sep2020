c8f23192eec0b78ba19b56d4ff5fd815046f6236
kbprabhukumar <kbprabhukumar@gmail.com>
2020-09-27 19:08:54 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+
+const express = require('express'); //mvc framework expressJS
+const app = express(); // get a referecnes of express and assign to app variable
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log('App running on port %s', port);
+});
+
+/* http://localhost:8080/ */
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/login.html');
+});
+
+/* http://localhost:8080/login */
+app.post('/login', (req, res) => {
+    res.end('Login Success -1!');
+});
+
+
+/* http://localhost:8080/products */
+app.get('/products', (req, res) => {
+
+});
